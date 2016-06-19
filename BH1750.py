f82ac96774768025e31cb71453692d17c668880a
ElJulio <julius.brueckner@outlook.de>
2016-06-19 14:47:06 +0200

    Add files via upload

--- BH1750.py
+++ BH1750.py
@@ -19,9 +19,9 @@ def read_word(addr):
 
 def main():
     #Wake up the BH170
-    bus.write_byte_data(address,0x1)
+    bus.write_byte_data(address,0x1,0)
 
-    bus.write_byte_data(address,0x10)
+    bus.write_byte_data(address,0x10,0)
     time.sleep(0.24)
     print bus.read_byte_data(address,0x10)
 

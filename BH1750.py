1db117938a895e0f15880d49feb95fe9eafa2e8d
ElJulio <julius.brueckner@outlook.de>
2016-06-19 14:39:40 +0200

    Add files via upload

---
+++ BH1750.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import time
+import smbus
+
+#Address of the BH1750
+address = 0x23
+bus = smbus.SMBus(1)
+
+def read_byte(addr):
+    return bus.read_byte_data(address,addr)
+
+def read_word(addr):
+    high = bus.read_byte_data(address,addr)
+    low = bus.read_byte_data(address,addr+1)
+    val = (high << 8)+ low
+    return val
+
+
+def main():
+    #Wake up the BH170
+    bus.write_byte_data(address,0x1)
+
+    bus.write_byte_data(address,0x10)
+    time.sleep(0.24)
+    print bus.read_byte_data(address,0x10)
+
+if __name__ == "__main__":
+    main()
